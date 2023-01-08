f868c55b29a711501708f1ae54b1df2b307ea557
Mikaela-D <G00401392@atu.ie>
2023-01-08 22:22:40 +0000

    Added code calculation with try catch

--- src/main/java/atu/ie/TryCatch.java
+++ src/main/java/atu/ie/TryCatch.java
@@ -1,5 +1,23 @@
 package atu.ie;
 
-public class TryCatch {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class TryCatch {
+    static final int COMMISSION_RATE = 20;
+
+    public static void main(String[] args) {
+        try
+        {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Enter your sales: ");
+            double sales = keyboard.nextDouble();
+            double commission = sales * COMMISSION_RATE;
+            System.out.printf("Your commission: €%,.2f\n", commission);
+        } catch(InputMismatchException e)
+        {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Enter a valid numeric value.");
+        }
+    }
 }
